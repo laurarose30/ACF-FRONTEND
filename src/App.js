e0c4503413c2bcf0945a9f74580e19219068f549
Laura Rose Mary Hansen <laurarosemaryhansen@gmail.com>
2022-01-26 12:06:06 +0000

    style

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
 
   return (
     <>
-     <Navbar  id="nav" expand="lg" variant="light" bg="light">
+     <Navbar id="nav" expand="lg" variant="light" bg="light">
         <Container>
           <Navbar.Brand>ACF Training</Navbar.Brand>
           
